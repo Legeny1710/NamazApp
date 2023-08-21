f030d1538a1fdb267ae7c696e3535aaf76d3b3c8
Yusuf Kula <yvk2045@gmail.com>
2023-08-21 12:18:13 +0100

    Fixed search functionality

--- main.js
+++ main.js
@@ -9,6 +9,32 @@ let asr = document.getElementById("Asr")
 let maghrib = document.getElementById("Maghrib")
 let isha = document.getElementById("Isha")
 
+//input box
+let LocationInput = document.getElementById("LocationInput")
+let searchbtn = document.getElementById("searchbtn")
+
+let locationText = document.getElementById("location")
+
+
+let Chosenlocation = ""
+
+searchbtn.addEventListener('click', function() {
+    Chosenlocation = LocationInput.value
+    locationText.textContent = Chosenlocation
+    fetch(`http://api.aladhan.com/v1/calendarByCity/${current_day.year}/${current_day.month+1}?city=${Chosenlocation}&country=United Kingdom&method=13`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.data[current_day.day - 1])
+        fajr.textContent = (data.data[current_day.day]['timings']['Fajr'].slice(0, 5))
+        sun.textContent = (data.data[current_day.day]['timings']['Sunrise'].slice(0, 5))
+        dhuhr.textContent = (data.data[current_day.day]['timings']['Dhuhr'].slice(0, 5))
+        asr.textContent = (data.data[current_day.day]['timings']['Asr'].slice(0, 5))
+        maghrib.textContent = (data.data[current_day.day]['timings']['Maghrib'].slice(0, 5))
+        isha.textContent = (data.data[current_day.day]['timings']['Isha'].slice(0, 5))
+    })
+
+})
+
 let dateText = document.getElementById("date")
 
 
@@ -34,16 +60,5 @@ dateText.textContent = current_day.ReturnDate()
 
 
 
-fetch(`http://api.aladhan.com/v1/calendarByAddress/${current_day.year}/${current_day.month+1}?address=Sultanahmet Mosque, Istanbul, Turkey&method=2`)
-.then(response => response.json())
-.then(data => {
-    console.log(data.data[current_day.day - 1])
-    fajr.textContent = (data.data[current_day.day]['timings']['Fajr'].slice(0, 5))
-    sun.textContent = (data.data[current_day.day]['timings']['Sunrise'].slice(0, 5))
-    dhuhr.textContent = (data.data[current_day.day]['timings']['Dhuhr'].slice(0, 5))
-    asr.textContent = (data.data[current_day.day]['timings']['Asr'].slice(0, 5))
-    maghrib.textContent = (data.data[current_day.day]['timings']['Maghrib'].slice(0, 5))
-    isha.textContent = (data.data[current_day.day]['timings']['Isha'].slice(0, 5))
-})
 
 
